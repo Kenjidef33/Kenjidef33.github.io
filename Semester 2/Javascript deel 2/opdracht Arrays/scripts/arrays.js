37e3e83b92499c2a9737fa405c33a0a6cd697489
Kenjidef <kenjidef@gmail.com>
2024-02-22 12:49:18 +0100

    Create arrays.js

---
+++ Semester 2/Javascript deel 2/opdracht Arrays/scripts/arrays.js
@@ -0,0 +1,19 @@
+var familieleden = ["Jan", "Piet", "Kevin", "Maria", "Louise", "Tomas"];
+
+function voegNaamToe(array, naam) {
+    array.push(naam);
+}
+
+console.log("Het aantal elementen in de array:", familieleden.length);
+
+console.log("Eerste element:", familieleden[0]);
+console.log("Derde element:", familieleden[2]);
+console.log("Vijfde element", familieleden[4]);
+
+var nieuweNaam = prompt("Voer een nieuwe naam in:");
+voegNaamToe(familieleden, nieuweNaam);
+console.log("Array na het toevoegen van de nieuwe naam:", familieleden);
+
+var familieledenString = familieleden.join(",");
+console.log("Array is geconverteerd naar een String:", familieledenString);
+
