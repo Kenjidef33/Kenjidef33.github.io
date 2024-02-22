18f4aef56564d51d3a87e4337b63a1d25d6a3248
Kenjidef <kenjidef@gmail.com>
2024-02-22 14:59:03 +0100

    Update Opdracht kopieer.js

---
+++ Semester 2/Javascript deel 2/Opdracht 3/scripts/Opdracht kopieer.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let btnKopieer = document.getElementById("btnKopieer");
+    btnKopieer.addEventListener("click", kopieer);
+});
+
+const kopieer = () => {
+    let txtInput = document.getElementById("txtInput");
+    let tekst = txtInput.value;
+    let txtOutput = document.getElementById("txtOutput");
+    txtOutput.textContent = tekst;
+};
