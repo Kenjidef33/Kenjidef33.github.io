75e28f4fa0fa195b8a2b1bab7c0c98ad0e87bd5e
Kenjidef <kenjidef@gmail.com>
2024-02-22 13:59:48 +0100

    Create Opdracht 4.js

---
+++ Semester 2/Javascript deel 2/Opdracht 3/scripts/Opdracht 4.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setup();
+});
+
+function setup() {
+    let pElement = document.getElementById("txtOutput");
+    pElement.innerHTML = "Welkom!";
+}
