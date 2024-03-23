5140cfb3dd07623c723300b5e6a3493622b4dab8
Kenjidef <kenjidef@gmail.com>
2024-03-23 23:54:24 +0100

    Update Gemeente.js

--- semester 2 labo 5/scripts/Gemeente.js
+++ semester 2 labo 5/scripts/Gemeente.js
@@ -14,11 +14,9 @@ gemeenten.sort(function(a, b) {
     return a.localeCompare(b);
 });
 
-// Populate select with sorted gemeenten
 gemeenten.forEach(function(gemeente) {
     const option = document.createElement('option');
     option.text = gemeente;
     option.value = gemeente;
     selectElement.add(option);
 });
-
